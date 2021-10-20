a10a8e04ca610a1120854d8022737e27b9e46d78
Aditya <pssanthoshaditya2000@gmail.com>
2021-10-20 17:58:43 +0530

    Installed libraries

--- weatherscraper/views.py
+++ weatherscraper/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def IndexView(request):
-    
+    import urllib3
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'https://air-quality.com/place/india/gurugram/d2853e61?lang=en&standard=aqi_us')
+    html_doc = r.data
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    city_title = soup.find(class_='detail-main').find(class_='detail-title').h2.text
+    latest_time = soup.find(class_='detail-main').find(class_='update-time').text.strip()
+    pollutant_name = soup.find(class_="pollutants").find(class_="name").text
+    pollutant_unit = soup.find(class_="pollutants").find(class_="unit").text
+    pollutant_value = soup.find(class_="pollutants").find(class_="value").text
