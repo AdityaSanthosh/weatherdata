86524f474901170388b320db1c42bc4701edc814
Aditya <pssanthoshaditya2000@gmail.com>
2021-10-20 18:02:14 +0530

    temp commit

--- weatherscraper/models.py
+++ weatherscraper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class CityData(models.Model):
+    city_title = models.CharField()
+    latest_time = models.CharField()
+    pollutant_name = models.CharField()
+    pollutant_unit = models.CharField()
+    pollutant_value = models.CharField()
